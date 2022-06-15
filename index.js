41417edd1c85002b690e4eb4a6f14fc9c361933b
GordonGichini <gordongichini1@gmail.com>
2022-06-15 20:11:45 +0300

    commit tree 64ac1d07108665dad5a25e81925c2f4e38d9a0cb

---
+++ index.js
@@ -0,0 +1,16 @@
+//let a= 2000, b = 100;
+//const divide = (a / b ); => {square (a * a)};
+    const divide = (a = 2000, b = 100) => {
+        return a / b;
+    }
+    const num = 6;
+    const square = function (x){
+        return x*x;
+    }
+    const add = function(x,y){
+        return x+y;
+    }
+    //const add = (x) => {
+     //   console.log(`Adding ${x}`);
+     //   return x + x;
+    
